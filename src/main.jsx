736499896babbee1b2a758dfa714765da5f2841f
Devmacy <devmaccyy@gmail.com>
2023-01-05 01:06:57 +0800

    feat: 新增memo性能优化

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './App.css'
 import MemoTest from "./MemoTest";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
